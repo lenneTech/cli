f23260a48843aa2658981ca022c6b229d8a3241a
Kai Haase <kai.haase@lenne.tech>
2023-02-25 05:35:34 +0100

    Updated API Server to version 9.2.8

---
+++ src/commands/server/create-secret.ts
@@ -0,0 +1,24 @@
+import * as crypto from 'crypto';
+import { GluegunCommand } from 'gluegun';
+import { ExtendedGluegunToolbox } from '../../interfaces/extended-gluegun-toolbox';
+
+/**
+ * Open regex tools in browser
+ */
+const NewCommand: GluegunCommand = {
+  name: 'createSecret',
+  alias: ['cs'],
+  description: 'Create a new secret string (for JWT config)',
+  hidden: false,
+  run: async (toolbox: ExtendedGluegunToolbox) => {
+    const {
+      print: { success },
+    } = toolbox;
+    success(crypto.randomBytes(512).toString('base64'));
+
+    // For tests
+    return `secret created`;
+  },
+};
+
+export default NewCommand;
